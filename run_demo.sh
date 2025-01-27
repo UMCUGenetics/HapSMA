5e3f8c7c1af5cbad23cd39d42d2d34cc78739c15
melferink <m.elferink@umcutrecht.nl>
2025-01-27 08:30:45 +0100

    new dema data bam

--- run_demo.sh
+++ run_demo.sh
@@ -16,7 +16,7 @@ $workflow_path/tools/nextflow/nextflow run \
     $workflow_path/SMA.nf \
     -c $workflow_path/SMA.config \
     -c $workflow_path/SMA_demodata.config \
-    --input_path $workflow_path/demodata/test_ont_sub.bam \
+    --input_path $workflow_path/demodata/HG02071_demo.bam \
     --genome_fasta $workflow_path/demodata/chr5.fa \
     --calling_target_bed $workflow_path/demodata/paraphase_SNPs_UCSC_liftover_T2T_CHM13.bed \
     --homopolymer_bed $workflow_path/demodata/hs1_hm_20a_22a_3homopolymer_chr5demo.bed \
@@ -24,7 +24,7 @@ $workflow_path/tools/nextflow/nextflow run \
     --calling_target_region "chr5:71274893-71447410" \
     --phaseset_region "chr5:71392465-71409463" \
     --start bam_single_remap \
-    --ploidy 3 \
+    --ploidy 4 \
     --sample_id test \
     --single_bam_type path \
     --workflowpath $workflow_path \
